61bac4ca1ca33226e2b4a8c8c9ce53a3cc8f48c5
josecruzado21 <josecruzado2103@gmail.com>
2021-05-30 01:12:48 -0500

    Arreglando formato de validaciones

--- src/routes/main.js
+++ src/routes/main.js
@@ -13,8 +13,8 @@ const {check} = require('express-validator');
 //Validaciones
 const validacionesRegister = [
     check('name')
-    .isLength({min:2}).withMessage('Tu nombre debe tener al menos 2 carácteres').bail()
-        .notEmpty().withMessage('Debes completar el campo nombre!'),
+    .notEmpty().withMessage('Debe completar el campo nombre!')
+    .isLength({min:2}).withMessage('Tu nombre debe tener al menos 2 carácteres').bail(),
         
    
     check('lastName').notEmpty().withMessage('Debes completar el campo apellido!'),
