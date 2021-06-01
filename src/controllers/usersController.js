41ce541fb0cfab0c5a257d5c20df41c844ee0ee5
Jaime Rivera <riverac85@gmail.com>
2021-05-31 23:17:41 -0500

    ajuste error registro

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -129,7 +129,9 @@ const usersController = {
                             msg: 'Ya existe una cuenta asociada a este correo'
                         },
                     },
-                    oldData: req.body
+                    oldData: req.body,
+                    errorMailExist:null,
+                    errorImage:null
                 });
             }
 
@@ -142,7 +144,9 @@ const usersController = {
                             msg: 'Debes subir solo archivos de imagen (JPG, PNG, GIF)'
                         },
                     },
-                    oldData: req.body
+                    oldData: req.body,
+                    errorMailExist:null,
+                    errorImage:null
                 });
             }
 
