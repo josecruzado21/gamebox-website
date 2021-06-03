5b8099787906c9ec757fc5cd0037978a685e90b1
Jaime Rivera <riverac85@gmail.com>
2021-06-03 14:29:00 -0500

    ajuste modal añadir a carrito

--- public/js/productList.js
+++ public/js/productList.js
@@ -35,6 +35,7 @@ addToCartButtons.forEach(function (currentBtn) {
 
         cart(productToAdd, userId);
 
+        modal.style.display = "block";
     })
 
 })
@@ -53,9 +54,9 @@ var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
 
 
 // When the user clicks on <span> (x), close the modal
