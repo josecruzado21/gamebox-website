82e9e2356f92a15f1769a89e1ddb5dea68857d7b
Jaime Rivera <riverac85@gmail.com>
2021-06-03 13:11:38 -0500

    integracion banner principal con base de datos

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,12 +1,51 @@
 const path = require('path');
+const db = require('../database/models');
+const { QueryTypes, Sequelize } = require('sequelize');
+const Op = Sequelize.Op;
+const Product = db.Product;
+const Category = db.Category;
+
+
 const mainController = {
     home: (req, res) => {
         let title = 'Gamebox | Videojuegos y mas';
-        res.render('pages/index', 
-        {
-            'title': title,
-            'user' : req.session.userLogged,
-        });
+     
+        Product.findAll({limit : 4,
+            include: [{
+                model:Category,
+                as : 'categories',
+         
+            } ],
+            where: {
+                homeTags: {
+                    [Op.like]: '%banner%'
+                  }
+            },
+            order: [
+                ['id', 'DESC']
+               
+            ],
+        })
+            .then(products => {
+                console.log("products banner")
+              console.log(products)
+             // res.json(products)
+
+              res.render('pages/index', 
+              {
+                  'title': title,
+                  'user' : req.session.userLogged,
+                  productsBanner:products
+              });
+
+            })
+
+     
+        // res.render('pages/index', 
+        // {
+        //     'title': title,
+        //     'user' : req.session.userLogged,
+        // });
     },
 };
 
